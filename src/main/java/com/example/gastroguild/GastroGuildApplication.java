7c28c0386bdfdcb7853f03d9843824e6d5d6d614
MARVINAKI <kosakbig@gmial.com>
2023-12-10 14:18:54 +0500

    Initial commit

---
+++ src/main/java/com/example/gastroguild/GastroGuildApplication.java
@@ -0,0 +1,13 @@
+package com.example.gastroguild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GastroGuildApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GastroGuildApplication.class, args);
+	}
+
+}
